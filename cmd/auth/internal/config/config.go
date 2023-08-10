f8ed1748b0cc9cccbd0511966b79793feeaa921a
MTimofei <work.tim@rambler.ru>
2023-08-10 15:38:05 +0300

    create config package

--- cmd/auth/internal/config/config.go
+++ cmd/auth/internal/config/config.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"encoding/json"
+	"flag"
+	"os"
+)
+
+type Config struct {
+	Evn    string `json:"env"`
+	Server struct {
+		Port string `json:"port"`
+		Host string `json:"host"`
+	} `json:"server"`
+}
+
+var Cng Config
+
+var (
+	env  = flag.String("EVN", "", "env")
+	port = flag.String("PORT", "", "port")
+	host = flag.String("HOST", "", "host")
+)
+
+// читаем содержимое конфиг файла
+// если возникла ошибка паникуем
+func mostParseFile(pathFile string, cng *Config) {
+	f, err := os.Open(pathFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = json.NewDecoder(f).Decode(cng)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// читаем флаги и заменяем значения, есле указаны
+func parseFlags(cng *Config) {
+	flag.Parse()
+	switch {
+	case *env != "":
+		cng.Evn = *env
+	case *port != "":
+		cng.Server.Port = *port
+	case *host != "":
+		cng.Server.Host = *host
+	}
+}
+
+func init() {
+	mostParseFile("config/auth/config.json", &Cng)
+	parseFlags(&Cng)
+}
