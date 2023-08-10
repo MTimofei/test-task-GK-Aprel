1e5b5f7721f4c9f211ce3a22634a0b8cda8cb1c2
MTimofei <work.tim@rambler.ru>
2023-08-10 21:10:11 +0300

    create router

--- cmd/auth/internal/httpserver/router.go
+++ cmd/auth/internal/httpserver/router.go
@@ -1,1 +1,24 @@
 package httpserver
+
+import (
+	"net/http"
+
+	"github.com/auth-api/cmd/auth/internal/httpserver/public"
+	"github.com/auth-api/cmd/auth/internal/httpserver/secret"
+)
+
+const (
+	endpointClearAudit = "/audit/clear"
+	endpointAudit      = "/audit"
+	endpointAuth       = "/auth"
+)
+
+func router() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(endpointClearAudit, secret.HandlerClearAudit)
+	mux.HandleFunc(endpointAudit, secret.HandlerAudit)
+	mux.HandleFunc(endpointAuth, public.HandlerAuth)
+
+	return mux
+}
